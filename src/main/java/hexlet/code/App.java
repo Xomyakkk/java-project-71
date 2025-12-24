fdb6613d9ea8e8211a90d107ab20c9abae2e54b2
Alex <megaxomyak@gmail.com>
2025-12-24 18:17:47 +0300

    Add Option --format

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Command(
         name = "gendiff",
@@ -10,6 +11,16 @@ import picocli.CommandLine.Command;
         description = "Compares two configuration files and shows a difference."
 )
 public class App implements Runnable {
+
+    /**
+     * Формат вывода результата.
+     *
+     * @apiNote По умолчанию используется «stylish».
+     */
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format")
+    private String format = "stylish";   // значение по умолчанию
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
