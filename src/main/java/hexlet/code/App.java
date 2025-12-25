9752362f9683e648e353022dd18cccc8a80290f7
Alex <megaxomyak@gmail.com>
2025-12-25 09:16:46 +0300

    Add Parameters

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,6 +3,9 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 @Command(
         name = "gendiff",
@@ -21,6 +24,12 @@ public class App implements Runnable {
             description = "output format [default: stylish]",
             paramLabel = "format")
     private String format = "stylish";   // значение по умолчанию
+
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
